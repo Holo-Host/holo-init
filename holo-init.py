90153bdbf84ebd5c614ab841d99e1d9c77a4b04a
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-09-20 01:11:19 +0000

    Format via autopep8

--- holo-init.py
+++ holo-init.py
@@ -4,27 +4,34 @@ import retry
 import subprocess
 import sys
 
+
 def holochain_keygen(path):
     return subprocess.run(['hc', 'keygen', '-np', path, '-q'], capture_output=True) \
         .stdout \
         .split(b'\n')[0] \
         .decode('utf-8')
 
+
 HOLO_INIT_KEY = "wbfGXvzmLk83bUmR"
+
 
 def zato_request(endpoint, payload):
     return requests.post('http://proxy.holohost.net/zato' + endpoint,
-            headers={'Holo-Init': HOLO_INIT_KEY},
-            json=payload).json()
+                         headers={'Holo-Init': HOLO_INIT_KEY},
+                         json=payload).json()
+
 
 def zato_setup_dns(public_key):
     return zato_request('/holo-init-cloudflare-dns-create', {'pubkey': public_key})
 
+
 def zato_setup_zerotier(zerotier_address):
     return zato_request('/holo-zt-auth', {'member_id': zerotier_address})
 
+
 def zato_setup_zerotier_address():
     return zato_setup_zerotier(zerotier_address())
+
 
 def zato_setup_proxy(public_key, ipv4):
     return zato_request('/holo-init-proxy-service-create', {
@@ -34,6 +41,7 @@ def zato_setup_proxy(public_key, ipv4):
         'port': 48080
     })
 
+
 def zato_setup_proxy_route(public_key, proxy_id):
     return zato_request('/holo-init-proxy-route-create', {
         'name': public_key + '.holohost.net',
@@ -42,19 +50,25 @@ def zato_setup_proxy_route(public_key, proxy_id):
         'service': proxy_id
     })
 
+
 def zerotier_run(args):
-    process = subprocess.run(['zerotier-cli', '-j'] + args, capture_output=True)
+    process = subprocess.run(['zerotier-cli', '-j'] +
+                             args, capture_output=True)
     return json.loads(process.stdout)
+
 
 def zerotier_info():
     return zerotier_run(['info'])
 
+
 def zerotier_address():
     return zerotier_info()['address']
+
 
 @retry.retry(IndexError, tries=10, delay=2, backoff=2)
 def zerotier_ipv4():
     return zato_setup_zerotier_address()['config']['ipAssignments'][0]
+
 
 def main(private_key_path):
     ipv4 = zerotier_ipv4()
@@ -65,6 +79,7 @@ def main(private_key_path):
     zato_setup_dns(public_key)
     res = zato_setup_proxy(public_key, ipv4)
     print(zato_setup_proxy_route(public_key, res['id'])['name'])
+
 
 if __name__ == "__main__":
     main(sys.argv[1])
